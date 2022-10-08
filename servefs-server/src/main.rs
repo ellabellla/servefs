8a5e8559f69bbd2f7016b3b1a0b5fba24e35b855
ellabellla <86757313+ellabellla@users.noreply.github.com>
2022-10-08 12:16:52 +1100

    + cli args

--- servefs-server/src/main.rs
+++ servefs-server/src/main.rs
@@ -1,10 +1,35 @@
 #[macro_use] extern crate rocket;
-use std::{path::{PathBuf}, str::FromStr};
-use rocket::{State, http::{ContentType}};
+use std::{path::{PathBuf}, str::FromStr, net::IpAddr, fs};
+use clap::{command, Parser};
+use rocket::{State, http::{ContentType}, Config};
 use servefs_lib::*;
 use sqlx::{Row, sqlite::SqliteRow};
 use tera::{Tera, Context};
-use std::str;
+use std::{str};
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+   /// Location of database
+   #[arg(short, long)]
+   db: Option<String>,
+
+   /// Location of templates directory
+   #[arg(short, long)]
+   templates: Option<String>,
+
+   /// Location of directory template inside templates directory
+   #[arg(long)]
+   dir_template: Option<String>,
+
+   // Port
+   #[arg(short, long)]
+   port: Option<u16>,
+
+   // IP
+   #[arg(short, long)]
+   ip: Option<String>,
+}
 
 async fn exec(ext: &str, command: &str) -> Option<(ContentType, Vec<u8>)>{
     tokio::process::Command::new("bash")
@@ -41,7 +66,7 @@ async fn render_file(ext: &str, data: String, ftype: String) -> Option<(ContentT
     }
 }
 
-async fn render_dir(parent: &Directory, files: Vec<SqliteRow>, dirs: Vec<SqliteRow>, tera: &State<Tera>) -> Option<(ContentType, Vec<u8>)> {
+async fn render_dir(parent: &Directory, files: Vec<SqliteRow>, dirs: Vec<SqliteRow>, tera: &State<Tera>, dir_template: &State<String>) -> Option<(ContentType, Vec<u8>)> {
     let mut dirs = dirs
         .iter()
         .map(|row| row.get::<String, &str>("directory"))
@@ -60,7 +85,7 @@ async fn render_dir(parent: &Directory, files: Vec<SqliteRow>, dirs: Vec<SqliteR
     context.insert("dirs", &dirs);
     context.insert("files", &files);
     context.insert("parent", &parent.path);
-    let html = tera.render("directory.html", &context).ok()?;
+    let html = tera.render(dir_template, &context).ok()?;
 
     Some((ContentType::HTML, html.as_bytes().to_vec()))
 }
@@ -76,7 +101,7 @@ fn get_ext(name: &str) -> String {
 }
 
 #[get("/<path..>")]
-async fn get_fs(path: PathBuf, fs_conn: &State<FSConnection>, tera: &State<Tera>) -> Option<(ContentType, Vec<u8>)> {
+async fn get_fs(path: PathBuf, fs_conn: &State<FSConnection>, tera: &State<Tera>, dir_template: &State<String>) -> Option<(ContentType, Vec<u8>)> {
     match fs_conn.resolve_path(path).await {
         Ok(fs_type) => match fs_type {
             FSType::File(file) => match file.read(&fs_conn).await {
@@ -84,7 +109,7 @@ async fn get_fs(path: PathBuf, fs_conn: &State<FSConnection>, tera: &State<Tera>
                 Err(_) => None,
             },
             FSType::Directory(dir) => match dir.contents(fs_conn).await {
-                Ok((files, dirs)) => render_dir(&dir, files, dirs, tera).await,
+                Ok((files, dirs)) => render_dir(&dir, files, dirs, tera, dir_template).await,
                 Err(_) => None,
             },
         },
@@ -92,10 +117,90 @@ async fn get_fs(path: PathBuf, fs_conn: &State<FSConnection>, tera: &State<Tera>
     }
 }
 
-
 #[launch]
 async fn servefs() -> _ {
-    let tera = match Tera::new("templates/**/*") {
+    let default_config_dir = "servefs/";
+    let default_db_prefix = "sqlite://";
+    let default_template_file = "directory.html";
+    let default_directory_template = r#"<h1>{{parent}}</h1>
+{% for dir in dirs %}
+    <a href="{{dir}}">dir</a></br>
+{% endfor %}
+{% for file in files %}
+    <a href="{{parent}}{{file}}">{{file}}</a></br>
+{% endfor %}"#;
+
+    let mut config = dirs::config_dir().expect("Could not find config path.");
+    config.push(default_config_dir);
+    fs::create_dir_all(&config).expect("Couldn't find a default config location");
+
+    let args = Args::parse();
+    
+    let db_loc = match args.db {
+        Some(db_loc) => db_loc,
+        None => {
+            let mut db_loc = config.clone();
+            db_loc.push("fs.db");
+            let db_loc = db_loc.to_str().expect("Couldn't find a default db location").to_string();
+            format!("{}{}", default_db_prefix, db_loc)
+        },
+    };
+
+    let mut template_path = config.clone();
+    template_path.push("templates/");
+    let template_loc = match args.templates {
+        Some(template_loc) => {
+            template_path = PathBuf::from_str(&template_loc).expect("Couldn't find template location");
+            let mut template_loc = template_path.clone();
+            template_loc.push("**/*");
+            template_loc.to_str().expect("Couldn't find a default template location").to_string()
+        },
+        None => {
+            let mut template_path = template_path.clone();
+            fs::create_dir_all(&template_path).expect("Couldn't find a default template location");
+            template_path.push("**/*");
+            template_path.to_str().expect("Couldn't find a default template location").to_string()
+        }
+    };
+
+    let dir_template_loc = match args.dir_template {
+        Some(dir_template_loc) => {
+            if !PathBuf::from_str(&dir_template_loc).expect("Couldn't find directory template").is_relative() {
+                println!("Directory template location must be relative");
+                ::std::process::exit(1);
+            }
+            template_path.push(&dir_template_loc);
+            if !template_path.is_file() {
+                println!("Couldn't find directory template");
+                ::std::process::exit(1);
+            }
+            dir_template_loc
+        },
+        None => {
+            template_path.push(&default_template_file);
+            if !template_path.is_file() {
+                println!("{:?}", template_path);
+                fs::write(template_path, default_directory_template).expect("Couldn't create default directory template");
+            }
+            default_template_file.to_string()
+        }
+    };
+
+    let mut rocket_config = Config::default();
+    if let Some(port) = args.port {
+        rocket_config.port = port;
+    }
+    if let Some(ip) = args.ip {
+        rocket_config.address = match IpAddr::from_str(&ip) {
+            Ok(ip) => ip,
+            Err(e) => {
+                println!("Parsing error(s): {}", e);
+                ::std::process::exit(1);
+            }
+        }
+    }
+
+    let tera = match Tera::new(&template_loc) {
         Ok(t) => t,
         Err(e) => {
             println!("Parsing error(s): {}", e);
@@ -103,9 +208,11 @@ async fn servefs() -> _ {
         }
     };
 
-    let fs_conn = FSConnection::new("sqlite://fs.db", "servefs_", true).await.unwrap();
+    let fs_conn = FSConnection::new(&db_loc, "servefs_", true).await.unwrap();
     rocket::build()
+        .configure(rocket_config)
         .manage(fs_conn)
         .manage(tera)
+        .manage(dir_template_loc)
         .mount("/", routes![get_fs])
 }
